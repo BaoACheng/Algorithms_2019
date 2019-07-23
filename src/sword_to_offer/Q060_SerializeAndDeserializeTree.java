a31bc1503892581adb9a84c292e3b6c181d638ca
caifeng <645324231@qq.com>
2019-07-23 14:13:07 +0800

    commit question 60

--- src/sword_to_offer/Q060_SerializeAndDeserializeTree.java
+++ src/sword_to_offer/Q060_SerializeAndDeserializeTree.java
@@ -14,13 +14,47 @@ public class Q060_SerializeAndDeserializeTree {
      *
      * Tips:
      */
-    public String Serialize(TreeNode root) {
 
-        return "";
+    public String Serialize(TreeNode root) {
+        if (root == null)
+            return "";
+        String serialStr = "";
+        serialStr = SerializeCore(root, serialStr);
+        return serialStr;
     }
 
+    public String SerializeCore(TreeNode root, String str) {
+        if (root == null) {
+            str += "#!";
+            return str;
+        }
+        str += root.val;
+        str += "!";
+        str = SerializeCore(root.left, str);
+        str = SerializeCore(root.right, str);
+        return str;
+    }
+    int index = -1;
     public TreeNode Deserialize(String str) {
+        index++;
+        String[] strs = str.split("!");
+        TreeNode node = null;
+        System.out.println(strs[index]);
+        if(!strs[index].equals("#")){
+            node = new TreeNode(Integer.valueOf(strs[index]));
+            node.left = Deserialize(str);
+            node.right = Deserialize(str);
+        }
+        return node;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        Q060_SerializeAndDeserializeTree serialize = new Q060_SerializeAndDeserializeTree();
+        TreeNode root = TreeNode.getTree();
+        String res = serialize.Serialize(root);
+        System.out.println(res);
+        System.out.println();
+        TreeNode node = serialize.Deserialize(res);
+        TreeNode.inOrder(node);
     }
 }
